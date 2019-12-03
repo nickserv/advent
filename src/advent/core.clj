5e040dd1a5fc87e569e91e0774bbf4b7678cbf81
Nick McCurdy <nick@nickmccurdy.com>
2019-12-03 12:42:21 -0500

    Simplify process-opcodes

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -19,18 +19,17 @@
        (clojure.string/split (clojure.string/trim (slurp "src/advent/input2.txt")) #",")))
 
 (defn process-opcodes [opcodes position]
-  (cond
-    (= 1 (get opcodes position)) (process-opcodes (assoc opcodes
-                                                         (get opcodes (+ 3 position))
-                                                         (+ (get opcodes (get opcodes (+ 1 position)))
-                                                            (get opcodes (get opcodes (+ 2 position)))))
-                                                  (+ 4 position))
-    (= 2 (get opcodes position)) (process-opcodes (assoc opcodes
-                                                         (get opcodes (+ 3 position))
-                                                         (* (get opcodes (get opcodes (+ 1 position)))
-                                                            (get opcodes (get opcodes (+ 2 position)))))
-                                                  (+ 4 position))
-    :else opcodes))
+  (let [opcode (get opcodes position)
+        left (get opcodes (+ 1 position))
+        right (get opcodes (+ 2 position))
+        result (get opcodes (+ 3 position))
+        new-position (+ 4 position)]
+    (if (some #{opcode} [1 2])
+      (process-opcodes (assoc opcodes result ((if (= 1 opcode) + *)
+                                              (get opcodes left)
+                                              (get opcodes right)))
+                       new-position)
+      opcodes)))
 
 (defn -main [& args]
   (println (reduce + (map fuel masses)))
