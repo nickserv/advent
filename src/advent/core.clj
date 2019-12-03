96de3cb52bb8b6add747bde5775467d4c10f74fb
Nick McCurdy <nick@nickmccurdy.com>
2019-12-02 21:27:22 -0500

    Extract masses constant

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,10 +1,13 @@
 (ns advent.core
   (:gen-class))
 
+(def masses
+  (map #(Integer/parseInt %)
+       (clojure.string/split-lines (slurp "src/advent/input.txt"))))
+
 (defn fuel [mass]
-  (- (int (/ mass 3)) 2))
+  (- (int (/ mass 3))
+     2))
 
 (defn -main [& args]
-  (println (reduce +
-                   (map #(fuel (Integer/parseInt %))
-                        (clojure.string/split-lines (slurp "src/advent/input.txt"))))))
+  (println (reduce + (map fuel masses))))
