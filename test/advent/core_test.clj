f84d2c4422f7c40c16c39bf88813dda1a0e01b23
Nick McCurdy <nick@nickmccurdy.com>
2019-12-03 06:45:44 -0500

    Add tests

--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [advent.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fuel-test
+  (is (= 2 (fuel 12)))
+  (is (= 2 (fuel 14)))
+  (is (= 654 (fuel 1969)))
+  (is (= 33583 (fuel 100756))))
+
+(deftest additional-fuel-test
+  (is (= 2 (additional-fuel 14)))
+  (is (= 966 (additional-fuel 1969)))
+  (is (= 50346 (additional-fuel 100756))))
