ff20217254c02b21eb4066038f56f99800398512
Nick McCurdy <nick@nickmccurdy.com>
2019-12-02 20:37:36 -0500

    Solve day 1 part 1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,7 +1,10 @@
 (ns advent.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn fuel [mass]
+  (- (int (/ mass 3)) 2))
+
+(defn -main [& args]
+  (println (reduce +
+                   (map #(fuel (Integer/parseInt %))
+                        (clojure.string/split-lines (slurp "src/advent/input.txt"))))))
