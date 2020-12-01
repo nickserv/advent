54af3b2fcb32a94c66981d9b991279de00d1638d
Nick McCurdy <nick@nickmccurdy.com>
2020-12-01 15:02:55 -0500

    Solve day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("resources/1.txt")?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)?;
+
+    let numbers = buf
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<u32>>();
+
+    let mut i = 0;
+    while i < numbers.len() {
+        let mut j = i;
+        while j < numbers.len() {
+            let sum = numbers[i] + numbers[j];
+            if sum == 2020 {
+                let product = numbers[i] * numbers[j];
+                println!("{}", product);
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    Ok(())
 }
