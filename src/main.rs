cceaa9779bbe0c938e0e8ed2eee4f9f7055e1b1c
Nick McCurdy <nick@nickmccurdy.com>
2020-12-01 15:34:45 -0500

    Extract functions

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-fn main() -> io::Result<()> {
+fn read_numbers() -> io::Result<Vec<u32>> {
     let mut file = File::open("resources/1.txt")?;
     let mut buf = String::new();
     file.read_to_string(&mut buf)?;
-
-    let numbers = buf
+    Ok(buf
         .lines()
         .map(|line| line.parse().unwrap())
-        .collect::<Vec<u32>>();
+        .collect::<Vec<u32>>())
+}
 
+fn find_product(numbers: Vec<u32>) -> Option<u32> {
     let mut i = 0;
     while i < numbers.len() {
         let mut j = i;
@@ -19,12 +20,17 @@ fn main() -> io::Result<()> {
             let sum = numbers[i] + numbers[j];
             if sum == 2020 {
                 let product = numbers[i] * numbers[j];
-                println!("{}", product);
+                return Some(product);
             }
             j += 1;
         }
         i += 1;
     }
+    None
+}
 
+fn main() -> io::Result<()> {
+    let numbers = read_numbers()?;
+    println!("{}", find_product(numbers).unwrap());
     Ok(())
 }
