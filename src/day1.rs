5c2736cc86984acb94fa55f6de8fc3f81d6b4635
Nick McCurdy <nick@nickmccurdy.com>
2021-12-02 01:04:48 -0500

    Solve day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -8,14 +8,26 @@ fn parse_numbers() -> Vec<u32> {
         .collect()
 }
 
-fn count_comparisons(numbers: &[u32]) -> usize {
-    numbers
-        .windows(2)
-        .filter(|window| window[1] > window[0])
-        .count()
+fn count_comparisons<'a, T: Iterator<Item = &'a [u32]>>(numbers: T) -> usize {
+    numbers.filter(|window| window[1] > window[0]).count()
+}
+
+fn count_comparisons_1(numbers: &[u32]) -> usize {
+    count_comparisons(numbers.windows(2))
+}
+
+fn count_comparisons_2(numbers: &[u32]) -> usize {
+    count_comparisons(
+        numbers
+            .windows(3)
+            .map(|window| window.iter().sum())
+            .collect::<Vec<_>>()
+            .windows(2),
+    )
 }
 
 pub fn main() {
     let numbers = parse_numbers();
-    println!("{}", count_comparisons(&numbers));
+    println!("{}", count_comparisons_1(&numbers));
+    println!("{}", count_comparisons_2(&numbers));
 }
