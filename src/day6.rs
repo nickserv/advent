ffaf0255a74f48c6702ac1530e26c5f215c9000c
Nick McCurdy <nick@nickmccurdy.com>
2021-12-06 07:19:38 -0500

    Solve day 6

---
+++ src/day6.rs
@@ -0,0 +1,29 @@
+use std::fs::read_to_string;
+
+fn simulate(fish_counts: [i64; 9], days: i16) -> i64 {
+    let mut fish_counts = fish_counts;
+    for _ in 0..days {
+        let new_fish = fish_counts[0];
+        fish_counts.rotate_left(1);
+        fish_counts[6] += new_fish;
+        fish_counts[8] = new_fish;
+    }
+    fish_counts.iter().sum()
+}
+
+pub fn main() {
+    let fish_ages: Vec<usize> = read_to_string("resources/6.txt")
+        .unwrap()
+        .trim()
+        .split(',')
+        .flat_map(|fish| fish.parse())
+        .collect();
+
+    let mut fish_counts = [0; 9];
+    for fish_age in fish_ages {
+        fish_counts[fish_age] += 1;
+    }
+
+    println!("{}", simulate(fish_counts, 80));
+    println!("{}", simulate(fish_counts, 256));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod day1;
 mod day2;
+mod day6;
 
 fn main() {
     day1::main();
     day2::main();
+    day6::main();
 }
