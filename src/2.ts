dd77170c484b487353494c286e242c3deb885563
Nick McCurdy <nick@nickmccurdy.com>
2022-12-03 04:50:48 -0500

    Solve days 2-3

---
+++ src/2.ts
@@ -0,0 +1,101 @@
+import assert from "assert"
+import { readFile } from "fs/promises"
+import { sum } from "./util.js"
+
+enum Shape {
+  Rock,
+  Paper,
+  Scissors,
+}
+
+const shapes = new Map([
+  ["A", Shape.Rock],
+  ["B", Shape.Paper],
+  ["C", Shape.Scissors],
+  ["X", Shape.Rock],
+  ["Y", Shape.Paper],
+  ["Z", Shape.Scissors],
+])
+
+const winners = new Map([
+  [Shape.Rock, Shape.Paper],
+  [Shape.Paper, Shape.Scissors],
+  [Shape.Scissors, Shape.Rock],
+])
+
+function score1(otherShape: Shape, myShape: Shape) {
+  const shapeScore = new Map([
+    [Shape.Rock, 1],
+    [Shape.Paper, 2],
+    [Shape.Scissors, 3],
+  ]).get(myShape)!
+  if (winners.get(otherShape) === myShape) {
+    return shapeScore + 6
+  } else if (otherShape === myShape) {
+    return shapeScore + 3
+  } else {
+    return shapeScore
+  }
+}
+
+assert.strictEqual(score1(Shape.Rock, Shape.Paper), 8)
+assert.strictEqual(score1(Shape.Paper, Shape.Rock), 1)
+assert.strictEqual(score1(Shape.Scissors, Shape.Scissors), 6)
+
+console.log(
+  sum(
+    (await readFile("resources/2.txt", "utf8"))
+      .trim()
+      .split("\n")
+      .map(([otherShape, , myShape]) =>
+        score1(shapes.get(otherShape)!, shapes.get(myShape)!),
+      ),
+  ),
+)
+
+enum Result {
+  Win,
+  Draw,
+  Lose,
+}
+
+const results = new Map([
+  ["X", Result.Lose],
+  ["Y", Result.Draw],
+  ["Z", Result.Win],
+])
+
+function score2(otherShape: Shape, result: Result) {
+  const myShape = new Map([
+    [Result.Win, winners.get(otherShape)!],
+    [Result.Draw, otherShape],
+    [Result.Lose, winners.get(winners.get(otherShape)!)!],
+  ]).get(result)!
+  const shapeScore = new Map([
+    [Shape.Rock, 1],
+    [Shape.Paper, 2],
+    [Shape.Scissors, 3],
+  ]).get(myShape)!
+  if (result === Result.Win) {
+    return shapeScore + 6
+  } else if (result === Result.Draw) {
+    return shapeScore + 3
+  } else {
+    return shapeScore
+  }
+}
+
+assert.strictEqual(score2(Shape.Rock, Result.Draw), 4)
+assert.strictEqual(score2(Shape.Paper, Result.Lose), 1)
+assert.strictEqual(score2(Shape.Scissors, Result.Win), 7)
+
+console.log(
+  sum(
+    (await readFile("resources/2.txt", "utf8"))
+      .trim()
+      .split("\n")
+      .map(([otherShape, , result]) =>
+        score2(shapes.get(otherShape)!, results.get(result)!),
+      ),
+  ),
+)
---
+++ src/3.ts
@@ -0,0 +1,102 @@
+import assert from "assert"
+import { readFile } from "fs/promises"
+import { sum } from "./util.js"
+
+function parseSplitRucksack(string: string): [Set<string>, Set<string>] {
+  const pivot = string.length / 2
+  return [new Set(string.slice(0, pivot)), new Set(string.slice(pivot))]
+}
+
+function rucksackIntersection(rucksacks: Set<string>[]) {
+  return [
+    ...rucksacks
+      .reduce(
+        (intersection, rucksack) =>
+          new Set([...intersection].filter((char) => rucksack.has(char))),
+      )
+      .values(),
+  ][0]
+}
+
+const testLines = [
+  "vJrwpWtwJgWrhcsFMMfFFhFp",
+  "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+  "PmmdzqPrVvPwwTWBwg",
+  "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+  "ttgJtRGJQctTZtZT",
+  "CrZsJsPPZsGzwwsLwLmpwMDw",
+]
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[0])), "p")
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[1])), "L")
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[2])), "P")
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[3])), "v")
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[4])), "t")
+assert.strictEqual(rucksackIntersection(parseSplitRucksack(testLines[5])), "s")
+
+function priority(string: string) {
+  return (
+    string.charCodeAt(0) +
+    (string === string.toUpperCase()
+      ? 27 - "A".charCodeAt(0)
+      : 1 - "a".charCodeAt(0))
+  )
+}
+
+assert.strictEqual(priority("p"), 16)
+assert.strictEqual(priority("L"), 38)
+assert.strictEqual(priority("P"), 42)
+assert.strictEqual(priority("v"), 22)
+assert.strictEqual(priority("t"), 20)
+assert.strictEqual(priority("s"), 19)
+assert.strictEqual(
+  sum(
+    testLines.map((line) =>
+      priority(rucksackIntersection(parseSplitRucksack(line))),
+    ),
+  ),
+  157,
+)
+
+const lines = (await readFile("resources/3.txt", "utf8")).trim().split("\n")
+
+console.log(
+  sum(
+    lines.map((line) =>
+      priority(rucksackIntersection(parseSplitRucksack(line))),
+    ),
+  ),
+)
+
+assert.strictEqual(
+  rucksackIntersection(testLines.slice(0, 3).map((line) => new Set(line))),
+  "r",
+)
+assert.strictEqual(
+  rucksackIntersection(testLines.slice(3).map((line) => new Set(line))),
+  "Z",
+)
+
+function partition<T>(array: T[], n: number): T[][] {
+  return array.length
+    ? [array.slice(0, n), ...partition(array.slice(n), n)]
+    : []
+}
+
+assert.strictEqual(
+  sum(
+    partition(
+      testLines.map((line) => new Set(line)),
+      3,
+    ).map((rucksacks) => priority(rucksackIntersection(rucksacks))),
+  ),
+  70,
+)
+
+console.log(
+  sum(
+    partition(
+      lines.map((line) => new Set(line)),
+      3,
+    ).map((rucksacks) => priority(rucksackIntersection(rucksacks))),
+  ),
+)
