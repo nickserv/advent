25e7c4b0f1a6ecaaa62c643d7012c8a9e5ab11c6
Nick McCurdy <nick@nickmccurdy.com>
2022-12-04 00:49:42 -0500

    Remove test input trim

--- src/4.ts
+++ src/4.ts
@@ -36,14 +36,12 @@ function containedRanges(ranges: [Range, Range][]) {
 }
 
 const testRangePairs = parseInput(
-  `
-2-4,6-8
+  `2-4,6-8
 2-3,4-5
 5-7,7-9
 2-8,3-7
 6-6,4-6
-2-6,4-8
-  `.trim(),
+2-6,4-8`,
 )
 assert.strictEqual(containedRanges(testRangePairs), 2)
 
