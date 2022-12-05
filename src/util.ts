d7dfadf86b46e37f7a26c1cda5f77d813338cfe3
Nick McCurdy <nick@nickmccurdy.com>
2022-12-05 04:50:06 -0500

    Fix unwanted trimming of the beginning of input

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import { readFile } from "fs/promises"
 
 export async function input(day: number) {
-  return (await readFile(`resources/${day}.txt`, "utf8")).trim()
+  return (await readFile(`resources/${day}.txt`, "utf8")).trimEnd()
 }
 
 export function sum(numbers: number[]) {
