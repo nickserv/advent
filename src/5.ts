15017c8a9f13d09c1f039718c45f73b10240f414
Nick McCurdy <nick@nickmccurdy.com>
2022-12-05 04:50:14 -0500

    Solve day 5

---
+++ src/5.ts
@@ -0,0 +1,93 @@
+import assert from "assert"
+import { input } from "./util.js"
+
+function parseStacks(input: string): readonly string[] {
+  const lines: readonly string[] = input.split("\n").slice(0, -1)
+  return Array.from({ length: (lines[0].length - 3) / 4 + 1 }, (_, index) =>
+    lines
+      .map((line) => line[index * 4 + 1])
+      .filter((char) => char !== " ")
+      .reverse()
+      .join(""),
+  )
+}
+
+interface Move {
+  readonly quantity: number
+  readonly start: number
+  readonly end: number
+}
+
+function parseMove(input: string): Move {
+  const [, quantity, start, end] = /move (\d+) from (\d+) to (\d+)/
+    .exec(input)!
+    .map((number) => parseInt(number))
+  return { quantity, start, end }
+}
+
+function parseInput(input: string): [readonly string[], readonly Move[]] {
+  const [stacksInput, movesInput] = input.split("\n\n")
+  return [parseStacks(stacksInput), movesInput.split("\n").map(parseMove)]
+}
+
+const [testStacks, testMoves] = parseInput(`    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`)
+assert.deepStrictEqual(testStacks, ["ZN", "MCD", "P"])
+assert.deepStrictEqual(testMoves, [
+  { quantity: 1, start: 2, end: 1 },
+  { quantity: 3, start: 1, end: 3 },
+  { quantity: 2, start: 2, end: 1 },
+  { quantity: 1, start: 1, end: 2 },
+])
+
+function doMove(
+  stacks: readonly string[],
+  { quantity, start, end }: Move,
+  multiple = false,
+): readonly string[] {
+  if (quantity) {
+    const pivot = -(multiple ? quantity : 1)
+    const movedStacks = stacks.map((stack, index) => {
+      switch (index) {
+        case start - 1:
+          return stack.slice(0, pivot)
+        case end - 1:
+          return stack + stacks[start - 1].slice(pivot)
+        default:
+          return stack
+      }
+    })
+
+    if (multiple) return movedStacks
+    else return doMove(movedStacks, { quantity: quantity - 1, start, end })
+  } else return stacks
+}
+
+function doMoves(
+  stack: readonly string[],
+  moves: readonly Move[],
+  multiple = false,
+) {
+  return moves.reduce((stacks, move) => doMove(stacks, move, multiple), stack)
+}
+
+assert.deepStrictEqual(doMoves(testStacks, testMoves), ["C", "M", "PDNZ"])
+assert.deepStrictEqual(doMoves(testStacks, testMoves, true), ["M", "C", "PZND"])
+
+function message(stacks: readonly string[]): string {
+  return stacks.map((stack) => stack.slice(-1)).join("")
+}
+
+assert.strictEqual(message(["C", "M", "PDNZ"]), "CMZ")
+assert.strictEqual(message(["M", "C", "PZND"]), "MCD")
+
+const [stacks, moves] = parseInput(await input(5))
+console.log(message(doMoves(stacks, moves)))
+console.log(message(doMoves(stacks, moves, true)))
