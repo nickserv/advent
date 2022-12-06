fc0ef9fd9f992d6e856e70cad5a250dc8d417096
Nick McCurdy <nick@nickmccurdy.com>
2022-12-06 00:45:56 -0500

    Solve day 6

---
+++ src/6.ts
@@ -0,0 +1,33 @@
+import assert from "assert"
+import { input } from "./util.js"
+
+function findMarker(input: string, length: number) {
+  return Array.from(
+    { length: input.length },
+    (_, index) => index + length,
+  ).find(
+    (index) =>
+      new Set(input.slice(index - length, index).split("")).size === length,
+  )
+}
+
+const testInputs = [
+  "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+  "bvwbjplbgvbhsrlpgdmjqwftvncz",
+  "nppdvjthqldpwncqszvftbrmjlhg",
+  "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+  "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+]
+assert.strictEqual(findMarker(testInputs[0], 4), 7)
+assert.strictEqual(findMarker(testInputs[1], 4), 5)
+assert.strictEqual(findMarker(testInputs[2], 4), 6)
+assert.strictEqual(findMarker(testInputs[3], 4), 10)
+assert.strictEqual(findMarker(testInputs[4], 4), 11)
+assert.strictEqual(findMarker(testInputs[0], 14), 19)
+assert.strictEqual(findMarker(testInputs[1], 14), 23)
+assert.strictEqual(findMarker(testInputs[2], 14), 23)
+assert.strictEqual(findMarker(testInputs[3], 14), 29)
+assert.strictEqual(findMarker(testInputs[4], 14), 26)
+
+console.log(findMarker(await input(6), 4))
+console.log(findMarker(await input(6), 14))
