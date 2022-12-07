6991a87af7a3cc1094edc4aa2d613612935543a2
Nick McCurdy <nick@nickmccurdy.com>
2022-12-07 09:20:29 -0500

    Refactor to OOP

--- src/7.ts
+++ src/7.ts
@@ -1,45 +1,94 @@
 import assert from "assert"
 import { input, sum } from "./util.js"
 
-class File {
-  constructor(private name: string, public size: number) {}
+type Command = string | Set<Entry>
+
+const Command = {
+  parse(input: string) {
+    return [
+      ...input.matchAll(/\$ (\w+)(?: (\w+|\/|..))?(?:\n([\w\s\.]+))?/g),
+    ].map(([, name, path, output]): Command => {
+      switch (name) {
+        case "cd":
+          return path
+        case "ls":
+          return new Set(
+            [...output.matchAll(/(dir|\d+) (.+)/g)].map(([, dirOrSize, name]) =>
+              dirOrSize === "dir"
+                ? new Dir(name)
+                : new File(name, parseInt(dirOrSize)),
+            ),
+          )
+        default:
+          throw new Error(`Unknown command ${name}`)
+      }
+    })
+  },
 }
 
-class Dir {
-  constructor(private name: string, public contents?: Set<File | Dir>) {}
+interface Entry {
+  name: string
+  size: number
+}
 
-  size(): number {
-    if (this.contents)
-      return sum(
-        [...this.contents].map((value) =>
-          value instanceof Dir ? value.size() : value.size,
-        ),
-      )
+class File implements Entry {
+  constructor(public name: string, public size: number) {}
+}
+
+class Dir implements Entry {
+  constructor(public name: string, public contents?: Set<Entry>) {}
+
+  static fromCommands(input: string) {
+    const stack: Dir[] = []
+    for (const command of Command.parse(input)) {
+      if (typeof command === "string") {
+        if (command === "..") stack.pop()
+        else {
+          const dir = new Dir(command)
+          stack.at(-1)?.contents!.add(dir)
+          stack.push(dir)
+        }
+      } else {
+        stack.at(-1)!.contents = new Set(
+          [...command].filter((value) => value instanceof File),
+        )
+      }
+    }
+    return stack[0]
+  }
+
+  get #dirs(): Set<Dir> {
+    return new Set(
+      [...this.contents!].flatMap((value) =>
+        value instanceof Dir ? [...value.#dirs, value] : [],
+      ),
+    )
+  }
+
+  get size() {
+    if (this.contents) return sum([...this.contents].map((value) => value.size))
     else return 0
   }
-}
 
-type Command = string | Set<File | Dir>
+  get sum() {
+    const limit = 100_000
 
-function parseCommands(input: string) {
-  return [
-    ...input.matchAll(/\$ (\w+)(?: (\w+|\/|..))?(?:\n([\w\s\.]+))?/g),
-  ].map(([, name, path, output]): Command => {
-    switch (name) {
-      case "cd":
-        return path
-      case "ls":
-        return new Set(
-          [...output.matchAll(/(dir|\d+) (.+)/g)].map(([, dirOrSize, name]) =>
-            dirOrSize === "dir"
-              ? new Dir(name)
-              : new File(name, parseInt(dirOrSize)),
-          ),
-        )
-      default:
-        throw new Error(`Unknown command ${name}`)
-    }
-  })
+    return sum(
+      [...this.#dirs].map((dir) => dir.size).filter((size) => size <= limit),
+    )
+  }
+
+  get bestSize() {
+    const total = 70_000_000
+    const required = 30_000_000
+    const needed = this.size + required - total
+
+    return Math.min(
+      ...[...this.#dirs]
+        .map((dir) => dir.size)
+        .filter((size) => size >= needed),
+    )
+  }
 }
 
 const testOutput = `$ cd /
@@ -65,7 +114,7 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k`
-assert.deepStrictEqual(parseCommands(testOutput), [
+assert.deepStrictEqual(Command.parse(testOutput), [
   "/",
   new Set([
     new Dir("a"),
@@ -92,28 +141,8 @@ assert.deepStrictEqual(parseCommands(testOutput), [
     new File("k", 7214296),
   ]),
 ] satisfies Command[])
-
-function tree(commands: Command[]) {
-  const stack: Dir[] = []
-  for (const command of commands) {
-    if (typeof command === "string") {
-      if (command === "..") stack.pop()
-      else {
-        const dir = new Dir(command)
-        stack.at(-1)?.contents!.add(dir)
-        stack.push(dir)
-      }
-    } else {
-      stack.at(-1)!.contents = new Set(
-        [...command].filter((value) => value instanceof File),
-      )
-    }
-  }
-  return stack[0]
-}
-
 assert.deepStrictEqual(
-  tree(parseCommands(testOutput)),
+  Dir.fromCommands(testOutput),
   new Dir(
     "/",
     new Set([
@@ -140,37 +169,8 @@ assert.deepStrictEqual(
     ]),
   ),
 )
+assert.deepStrictEqual(Dir.fromCommands(testOutput).sum, 95_437)
+assert.deepStrictEqual(Dir.fromCommands(testOutput).bestSize, 24_933_642)
 
-function dirs(dir: Dir): Set<Dir> {
-  return new Set(
-    [...dir.contents!].flatMap((value) =>
-      value instanceof Dir ? [...dirs(value), value] : [],
-    ),
-  )
-}
-
-function sumDirs(dir: Dir) {
-  const limit = 100_000
-
-  return sum(
-    [...dirs(dir)].map((dir) => dir.size()).filter((size) => size <= limit),
-  )
-}
-
-assert.deepStrictEqual(sumDirs(tree(parseCommands(testOutput))), 95_437)
-
-console.log(sumDirs(tree(parseCommands(await input(7)))))
-
-function bestDirSize(dir: Dir) {
-  const total = 70_000_000
-  const required = 30_000_000
-  const needed = dir.size() + required - total
-
-  return Math.min(
-    ...[...dirs(dir)].map((dir) => dir.size()).filter((size) => size >= needed),
-  )
-}
-
-assert.deepStrictEqual(bestDirSize(tree(parseCommands(testOutput))), 24_933_642)
-
-console.log(bestDirSize(tree(parseCommands(await input(7)))))
+console.log(Dir.fromCommands(await input(7)).sum)
+console.log(Dir.fromCommands(await input(7)).bestSize)
