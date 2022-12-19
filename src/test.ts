7b52e26fcff9b6d383b159b643d5a47505d7245c
Nick McCurdy <nick@nickmccurdy.com>
2022-12-19 08:57:03 -0500

    Run tests in parallel

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,10 @@
 import { readdir } from "fs/promises"
 import { basename, extname } from "path"
 
-const days = (await readdir("src"))
-  .map((path) => parseInt(basename(path, extname(path))))
-  .filter((day) => day)
-  .sort((a, b) => a - b)
-
-for (const day of days) await import(`./${day}.js`)
+const files = await readdir("src")
+await Promise.all(
+  files.map(async (path) => {
+    const day = parseInt(basename(path, extname(path)))
+    if (day) await import(`./${day}.js`)
+  }),
+)
