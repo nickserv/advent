bf94a6cf65f01b14c05d7c5b57d22bd1c07743ed
Nick McCurdy <nick@nickmccurdy.com>
2022-12-21 07:52:50 -0500

    Refactor day 21 to parse linked instructions

--- src/21.ts
+++ src/21.ts
@@ -3,19 +3,27 @@ import { input } from "./util.js"
 
 type Operator = "+" | "-" | "*" | "/"
 
-interface Operation {
+interface OperationDeclaration {
   type: Operator
   left: string
   right: string
 }
 
+type InstructionDeclaration = number | OperationDeclaration
+
+interface Operation {
+  type: Operator
+  left: Instruction
+  right: Instruction
+}
+
 type Instruction = number | Operation
 
 function parseInstructions(input: string) {
-  return new Map(
+  const declarationMap = new Map(
     input.split("\n").map((line) => {
       const [register, value] = line.split(": ")
-      const instruction: Instruction = parseInt(value) || {
+      const instruction: InstructionDeclaration = parseInt(value) || {
         type: value[5] as Operator,
         left: value.slice(0, 4),
         right: value.slice(7),
@@ -23,26 +31,47 @@ function parseInstructions(input: string) {
       return [register, instruction]
     }),
   )
+  const map = new Map<
+    string,
+    | number
+    | {
+        type: Operator
+        left?: Instruction
+        right?: Instruction
+      }
+  >(
+    [...declarationMap].map(([register, instruction]) => {
+      if (typeof instruction === "number") {
+        return [register, instruction]
+      } else {
+        return [register, { type: instruction.type }]
+      }
+    }),
+  )
+  for (const [register, value] of map) {
+    const declaration = declarationMap.get(register)!
+    if (typeof value !== "number" && typeof declaration !== "number") {
+      value.left = map.get(declaration.left) as Operation
+      value.right = map.get(declaration.right) as Operation
+    }
+  }
+  return map as Map<string, Instruction>
 }
 
-function evaluate(
-  instructions: Map<string, Instruction>,
-  register = "root",
-): number {
-  const value = instructions.get(register)!
-  if (typeof value === "number") {
-    return value
+function evaluate(instruction: Instruction): number {
+  if (typeof instruction === "number") {
+    return instruction
   } else {
-    const { type, left, right } = value
+    const { type, left, right } = instruction
     switch (type) {
       case "+":
-        return evaluate(instructions, left) + evaluate(instructions, right)
+        return evaluate(left) + evaluate(right)
       case "-":
-        return evaluate(instructions, left) - evaluate(instructions, right)
+        return evaluate(left) - evaluate(right)
       case "*":
-        return evaluate(instructions, left) * evaluate(instructions, right)
+        return evaluate(left) * evaluate(right)
       case "/":
-        return evaluate(instructions, left) / evaluate(instructions, right)
+        return evaluate(left) / evaluate(right)
     }
   }
 }
@@ -62,7 +91,7 @@ pppw: cczh / lfqf
 lgvd: ljgn * ptdq
 drzm: hmdt - zczc
 hmdt: 32`)
-assert.equal(evaluate(testInstructions), 152)
+assert.equal(evaluate(testInstructions.get("root")!), 152)
 
 const instructions = parseInstructions(await input(21))
-console.log(evaluate(instructions))
+console.log(evaluate(instructions.get("root")!))
