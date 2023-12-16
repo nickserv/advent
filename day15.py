78d2897842c668a0670293e6b0bc12cd6a63d0fb
Nick McCurdy <nick@nickmccurdy.com>
2023-12-15 23:35:46 -0500

    Solve day 15 part 2

--- day15.py
+++ day15.py
@@ -8,5 +8,29 @@ def custom_hash(steps: str | list[str]):
     return sum(map(custom_hash, steps))
 
 
+def powers(steps: list[str]):
+    lens_boxes: list[dict[str, int]] = [{} for _ in range(256)]
+    for step in steps:
+        if "=" in step:
+            label, _, focal_length = step.partition("=")
+            lens_boxes[custom_hash(label)][label] = int(focal_length)
+        elif step.endswith("-"):
+            label = step[:-1]
+            lens_boxes[custom_hash(label)].pop(label, None)
+        else:
+            raise ValueError(f"invalid step for power(): '{step}'")
+    return lens_boxes
+
+
+def total_power(steps: list[str]):
+    return sum(
+        (index + 1) * (slot + 1) * power
+        for index, box in enumerate(powers(steps))
+        for slot, power in enumerate(box.values())
+    )
+
+
 if __name__ == "__main__":
-    print(custom_hash(Path("resources/15.txt").read_text("utf8").rstrip().split(",")))
+    steps = Path("resources/15.txt").read_text("utf8").rstrip().split(",")
+    print(custom_hash(steps))
+    print(total_power(steps))
--- test_day15.py
+++ test_day15.py
@@ -1,37 +1,48 @@
 import unittest
 
-from day15 import custom_hash
+from day15 import custom_hash, powers, total_power
+
+STEPS = [
+    "rn=1",
+    "cm-",
+    "qp=3",
+    "cm=2",
+    "qp-",
+    "pc=4",
+    "ot=9",
+    "ab=5",
+    "pc-",
+    "pc=6",
+    "ot=7",
+]
 
 
 class Test(unittest.TestCase):
     def test_custom_hash(self):
         self.assertEqual(custom_hash("HASH"), 52)
-        self.assertEqual(custom_hash("rn=1"), 30)
-        self.assertEqual(custom_hash("cm-"), 253)
-        self.assertEqual(custom_hash("qp=3"), 97)
-        self.assertEqual(custom_hash("cm=2"), 47)
-        self.assertEqual(custom_hash("qp-"), 14)
-        self.assertEqual(custom_hash("pc=4"), 180)
-        self.assertEqual(custom_hash("ot=9"), 9)
-        self.assertEqual(custom_hash("ab=5"), 197)
-        self.assertEqual(custom_hash("pc-"), 48)
-        self.assertEqual(custom_hash("pc=6"), 214)
-        self.assertEqual(custom_hash("ot=7"), 231)
-        self.assertEqual(
-            custom_hash(
-                [
-                    "rn=1",
-                    "cm-",
-                    "qp=3",
-                    "cm=2",
-                    "qp-",
-                    "pc=4",
-                    "ot=9",
-                    "ab=5",
-                    "pc-",
-                    "pc=6",
-                    "ot=7",
-                ]
-            ),
-            1320,
-        )
+        self.assertEqual(custom_hash(STEPS[0]), 30)
+        self.assertEqual(custom_hash(STEPS[1]), 253)
+        self.assertEqual(custom_hash(STEPS[2]), 97)
+        self.assertEqual(custom_hash(STEPS[3]), 47)
+        self.assertEqual(custom_hash(STEPS[4]), 14)
+        self.assertEqual(custom_hash(STEPS[5]), 180)
+        self.assertEqual(custom_hash(STEPS[6]), 9)
+        self.assertEqual(custom_hash(STEPS[7]), 197)
+        self.assertEqual(custom_hash(STEPS[8]), 48)
+        self.assertEqual(custom_hash(STEPS[9]), 214)
+        self.assertEqual(custom_hash(STEPS[10]), 231)
+        self.assertEqual(custom_hash(STEPS), 1320)
+
+    def test_powers(self):
+        for index, box in enumerate(powers(STEPS)):
+            match index:
+                case 0:
+                    expected = {"rn": 1, "cm": 2}
+                case 3:
+                    expected = {"ot": 7, "ab": 5, "pc": 6}
+                case _:
+                    expected = {}
+            self.assertEqual(box, expected)
+
+    def test_total_power(self):
+        self.assertEqual(total_power(STEPS), 145)
