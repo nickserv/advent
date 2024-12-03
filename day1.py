e5a90a0b1122596fa845dfa5b029a19bd108d9ed
Nicky McCurdy <nick@nickmccurdy.com>
2024-12-02 21:58:14 -0500

    Skip unnecessary variable declarations to fix redefining names

--- day1.py
+++ day1.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 
 def parse_lists(string: str) -> tuple[list[int], list[int]]:
-    left, right = zip(*(map(int, line.split()) for line in string.splitlines()))
-    return (left, right)
+    return tuple(zip(*(map(int, line.split()) for line in string.splitlines())))
 
 
 def total_distance(left: list[int], right: list[int]):
@@ -17,6 +16,6 @@ def similarity_score(left: list[int], right: list[int]):
 
 
 if __name__ == "__main__":
-    left, right = parse_lists(Path("resources/1.txt").read_text("utf8"))
-    print(total_distance(left, right))
-    print(similarity_score(left, right))
+    lists = parse_lists(Path("resources/1.txt").read_text("utf8"))
+    print(total_distance(*lists))
+    print(similarity_score(*lists))
