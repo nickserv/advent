04b53a7a974ad8c7d6c42f74736b4a938666023b
Nicky McCurdy <nick@nickmccurdy.com>
2024-12-10 07:51:05 -0500

    Solve day 10 part 1 (with broken tests)

---
+++ day10.py
@@ -0,0 +1,51 @@
+from collections import deque
+from math import sqrt
+
+from utils import get_input
+
+
+class TopMap:
+    def __init__(self, string: str):
+        self.list = [
+            None if char == "." else int(char) for char in string if char != "\n"
+        ]
+
+    def trailheads(self):
+        return (index for index, value in enumerate(self.list) if value == 0)
+
+    def neighbors(self, position: int):
+        width = int(sqrt(len(self.list)))
+        if position >= width:
+            yield position - width
+        if position % width != 0:
+            yield position - 1
+        if position % width != width - 1:
+            yield position + 1
+        if position < len(self.list) - width:
+            yield position + width
+
+    def reachable(self, trailhead: int):
+        visited = {trailhead}
+        queue = deque([trailhead])
+        while queue:
+            current = queue.popleft()
+            if self.list[current] == 9:
+                yield current
+            for neighbor in self.neighbors(current):
+                if (
+                    self.list[neighbor] == self.list[current] + 1
+                    and neighbor not in visited
+                ):
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+    def score(self, trailhead: int):
+        return len(list(self.reachable(trailhead)))
+
+    def scores(self):
+        return (self.score(trailhead) for trailhead in self.trailheads())
+
+
+if __name__ == "__main__":
+    top_map = TopMap(get_input(10))
+    print(sum(top_map.scores()))
---
+++ test_day10.py
@@ -0,0 +1,117 @@
+import unittest
+
+from day10 import TopMap
+from utils import get_input
+
+SMALL_MAP, LARGE_MAP = (
+    TopMap(get_input(string))
+    for string in (
+        """
+        0123
+        1234
+        8765
+        9876
+        """,
+        """
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        """,
+    )
+)
+PARTIAL_MAPS = [
+    TopMap(get_input(string))
+    for string in (
+        """
+        ...0...
+        ...1...
+        ...2...
+        6543456
+        7.....7
+        8.....8
+        9.....9
+        """,
+        """
+        ..90..9
+        ...1.98
+        ...2..7
+        6543456
+        765.987
+        876....
+        987....
+        """,
+        """
+        10..9..
+        2...8..
+        3...7..
+        4567654
+        ...8..3
+        ...9..2
+        .....01
+        """,
+    )
+]
+
+
+class TestDay10(unittest.TestCase):
+    def test_map(self):
+        with self.subTest(map="small"):
+            self.assertEqual(
+                SMALL_MAP.list, [0, 1, 2, 3, 1, 2, 3, 4, 8, 7, 6, 5, 9, 8, 7, 6]
+            )
+
+    def test_map_trailheads(self):
+        with self.subTest(map="small"):
+            self.assertEqual(list(SMALL_MAP.trailheads()), [0])
+
+        with self.subTest(map="large"):
+            self.assertEqual(
+                list(LARGE_MAP.trailheads()), [2, 4, 20, 38, 42, 45, 48, 54, 57]
+            )
+
+    def test_map_neighbors(self):
+        with self.subTest(map="small"):
+            self.assertEqual(list(SMALL_MAP.neighbors(0)), [1, 4])
+            self.assertEqual(list(SMALL_MAP.neighbors(5)), [1, 4, 6, 9])
+
+    def test_map_reachable(self):
+        with self.subTest(map="small"):
+            self.assertEqual(list(SMALL_MAP.reachable(0)), [12])
+
+    def test_map_score(self):
+        with self.subTest(map="small"):
+            self.assertEqual(SMALL_MAP.score(0), 1)
+
+        with self.subTest(map="large"):
+            self.assertEqual(LARGE_MAP.score(2), 5)
+            self.assertEqual(LARGE_MAP.score(4), 6)
+            self.assertEqual(LARGE_MAP.score(20), 5)
+            self.assertEqual(LARGE_MAP.score(38), 3)
+            self.assertEqual(LARGE_MAP.score(42), 1)
+            self.assertEqual(LARGE_MAP.score(45), 3)
+            self.assertEqual(LARGE_MAP.score(48), 5)
+            self.assertEqual(LARGE_MAP.score(54), 3)
+            self.assertEqual(LARGE_MAP.score(57), 5)
+
+        with self.subTest(map="partial"):
+            self.assertEqual(PARTIAL_MAPS[0].score(3), 2)
+            self.assertEqual(PARTIAL_MAPS[1].score(4), 1)
+            self.assertEqual(PARTIAL_MAPS[1].score(47), 2)
+            self.assertEqual(PARTIAL_MAPS[2].score(3), 2)
+
+    def test_map_scores(self):
+        with self.subTest(map="small"):
+            self.assertEqual(list(SMALL_MAP.scores()), [1])
+
+        with self.subTest(map="large"):
+            self.assertEqual(list(LARGE_MAP.scores()), [5, 6, 5, 3, 1, 3, 5, 3, 5])
+
+        with self.subTest(map="partial"):
+            self.assertEqual(list(PARTIAL_MAPS[0].scores()), [2])
+            self.assertEqual(list(PARTIAL_MAPS[1].scores()), [1, 2])
+            self.assertEqual(list(PARTIAL_MAPS[2].scores()), [2])
