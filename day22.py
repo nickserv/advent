43356d20725e6f06dac373976dc7189b1a56e3ab
Nicky McCurdy <nick@nickmccurdy.com>
2024-12-22 11:47:58 -0500

    Optimize secret creation by replacing the generator with a list
    As a result, the secrets list is only created once and doesn't have to
    be computed again in later function calls.

--- day22.py
+++ day22.py
@@ -1,9 +1,7 @@
-from collections.abc import Iterator
-
 from utils import get_input
 
 
-def mix_prune(secret: int, value: int) -> int:
+def mix_prune(secret: int, value: int):
     # 16777216 is 2**24, equivalent to a bitmask of 0xFFFFFF
     return secret ^ value & 0xFFFFFF
 
@@ -15,17 +13,10 @@ def next_secret(secret: int):
     return secret
 
 
-def iter_secrets(secret: int) -> Iterator[int]:
-    for _ in range(2000):
-        secret = next_secret(secret)
-        yield secret
-
-
-def last[T](it: Iterator[T]):
-    *_, last = it
-    return last
+def secrets(secret: int):
+    return [secret, *(secret := next_secret(secret) for _ in range(2000))]
 
 
 if __name__ == "__main__":
-    secrets = [int(secret) for secret in get_input(22).splitlines()]
-    print(sum(last(iter_secrets(secret)) for secret in secrets))
+    all_secrets = [secrets(int(secret)) for secret in get_input(22).splitlines()]
+    print(sum(secrets[-1] for secrets in all_secrets))
