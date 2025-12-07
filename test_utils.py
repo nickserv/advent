ccafa3d9a0217d953619ef4110720d9fffb63bc0
Nicky McCurdy <nick@nickmccurdy.com>
2025-12-07 07:42:00 +0000

    Import utils from the top level in tests

--- test_utils.py
+++ test_utils.py
@@ -3,23 +3,27 @@ import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from utils import clean_string, parse_id_range, parse_lines, read_input
+import utils
 
 
 class TestUtils(unittest.TestCase):
     def test_clean_string(self):
-        self.assertEqual(clean_string("  hello world  "), "hello world")
-        self.assertEqual(clean_string("  \n  hello  \n  "), "hello")
-        self.assertEqual(clean_string("\n    indented\n    text\n"), "indented\ntext")
+        self.assertEqual(utils.clean_string("  hello world  "), "hello world")
+        self.assertEqual(utils.clean_string("  \n  hello  \n  "), "hello")
+        self.assertEqual(
+            utils.clean_string("\n    indented\n    text\n"), "indented\ntext"
+        )
 
     def test_parse_id_range(self):
-        self.assertEqual(parse_id_range("1-5"), range(1, 6))
-        self.assertEqual(parse_id_range("10-12"), range(10, 13))
-        self.assertEqual(parse_id_range("5-5"), range(5, 6))
+        self.assertEqual(utils.parse_id_range("1-5"), range(1, 6))
+        self.assertEqual(utils.parse_id_range("10-12"), range(10, 13))
+        self.assertEqual(utils.parse_id_range("5-5"), range(5, 6))
 
     def test_parse_lines(self):
-        self.assertEqual(parse_lines(int, "1\n2\n3"), [1, 2, 3])
-        self.assertEqual(parse_lines(str.upper, "hello\nworld"), ["HELLO", "WORLD"])
+        self.assertEqual(utils.parse_lines(int, "1\n2\n3"), [1, 2, 3])
+        self.assertEqual(
+            utils.parse_lines(str.upper, "hello\nworld"), ["HELLO", "WORLD"]
+        )
 
     def test_read_input(self):
         with TemporaryDirectory() as tmpdir:
@@ -32,7 +36,7 @@ class TestUtils(unittest.TestCase):
             original_cwd = os.getcwd()
             try:
                 os.chdir(tmpdir)
-                content = read_input(1)
+                content = utils.read_input(1)
                 self.assertEqual(content, "test content")
             finally:
                 os.chdir(original_cwd)
